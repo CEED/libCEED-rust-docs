97cda6b6be67c79fc356f9e7aee185036fcdc672
jeremylt <jeremylt@users.noreply.github.com>
2020-11-11 21:08:33 +0000

    deploy: CEED/libCEED@a80ccc8bd924ba7b1e35852acdf442fe89522562

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libceed"] = [{"text":"impl&lt;'a&gt; Debug for VectorOpt&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Debug for Vector","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorView&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorViewMut&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Debug for Ceed","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
