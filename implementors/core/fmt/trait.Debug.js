5a918649dc2f1b2b4ee7ad02941d3c227ba01f87
jeremylt <jeremylt@users.noreply.github.com>
2021-03-20 05:11:14 +0000

    deploy: CEED/libCEED@e15f9bd09af0280c89b79924fa9af7dd2e3e30be

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
 implementors["ex1_volume"] = [{"text":"impl Debug for Opt","synthetic":false,"types":[]}];
 implementors["ex2_surface"] = [{"text":"impl Debug for Opt","synthetic":false,"types":[]}];
-implementors["libceed"] = [{"text":"impl&lt;'a&gt; Debug for VectorOpt&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Debug for Vector","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorView&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorViewMut&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Debug for Ceed","synthetic":false,"types":[]}];
+implementors["libceed"] = [{"text":"impl&lt;'a&gt; Debug for VectorOpt&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for Vector&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorView&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Debug for VectorViewMut&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl Debug for CeedError","synthetic":false,"types":[]},{"text":"impl Debug for Ceed","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
